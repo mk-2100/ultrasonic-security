56a9ac46b80cea111c24cc722715493c0de20ace
Marla <marlakamil210@gmail.com>
2026-09-04 19:21:51 -0400

    Establish ESP8266 WiFi connection

---
+++ ESPnotify.ino
@@ -0,0 +1,24 @@
+#include <ESP8266WiFi.h>
+//#include <ESP8266WebServer.h>
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  WiFi.begin("ssid", "pass");
+
+  Serial.print("Connecting... ");
+
+  if (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+  }
+  
+  Serial.print("Connected! IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+
+}
